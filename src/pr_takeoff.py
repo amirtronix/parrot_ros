03c3571f45292cfe009083c1f951049519358e6e
amirtronics <amir.h.ebrahimnezhad@gmail.com>
2024-02-21 23:22:20 -0800

    Create pr_takeoff.py

---
+++ src/pr_takeoff.py
@@ -0,0 +1,28 @@
+#!/home/amirtronics/parrot/bin/python
+
+import rospy
+import olympe
+import os
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+
+DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
+
+def test_takeoff():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(TakeOff()).wait().success()
+    time.sleep(10)
+    assert drone(Landing()).wait().success()
+    drone.disconnect()
+
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('sphinx_takeoff_node', anonymous= False)
+    rospy.loginfo("Takeoff Initiated")
+
+    test_takeoff()
+
