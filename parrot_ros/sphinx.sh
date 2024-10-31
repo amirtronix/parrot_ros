388f8e2e8a481a7e87d051c27e0bbabdeaf8fee4
amirtronix <amir.h.ebrahimnezhad@gmail.com>
2024-10-30 17:37:20 -0700

    Fix sphinx firmware error

--- parrot_ros/sphinx.sh
+++ parrot_ros/sphinx.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl start firmwared.service
 
-gnome-terminal -- sphinx "/opt/parrot-sphinx/usr/share/sphinx/drones/anafi_ai.drone"::firmware="/home/amirtronics/Downloads/anafi2-pc.ext2.zip"::wifi_iface=""
+gnome-terminal -- sphinx "/opt/parrot-sphinx/usr/share/sphinx/drones/anafi_ai.drone"::firmware="https://firmware.parrot.com/Versions/anafi2/pc/%23latest/images/anafi2-pc.ext2.zip"::wifi_iface=""
 
 gnome-terminal -- parrot-ue4-empty -ams-path="DefaultPath,Pickup:*" -ams-path="RunPath,Jasper"
 
