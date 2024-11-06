8cdaad4fd4ec69fafd616a4430de06b1f2406271
amirtronix <amir.h.ebrahimnezhad@gmail.com>
2024-11-06 14:58:43 -0800

    Merge remote-tracking branch 'origin/dell' into main

--- container.bash
+++ container.bash
@@ -3,5 +3,4 @@
 source /opt/ros/noetic/setup.bash
 alias catkin="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.7m"
 export PYTHONPATH=$PYTHONPATH:/usr/src/ultralytics
-catkin
 source /catkin_ws/devel/setup.bash
